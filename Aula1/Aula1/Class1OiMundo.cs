7bab2f1bc3ec6114f5de5c5b987af5b62afb2cb0
RobsonSS83 <robson_superbi@yahoo.com.br>
2021-09-01 15:18:01 -0300

    Aula1
    Aula1

---
+++ Aula1/Aula1/Class1OiMundo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula1
+{
+    class Class1OiMundo
+    {
+        static void Main(string[] args)
+        {
+            var idade = 38;
+            bool isAdulto = true;
+            decimal valor = 10.60m;
+            double valor2 = 10.60;
+            char sigla = 'C';
+            string estado = "São Paulo";
+        }
+    }
+}
